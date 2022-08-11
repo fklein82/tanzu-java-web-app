bab3b6f335b0be521fdbe236bc0c03337e0dc661
Frédéric KLEIN <74149300+fklein82@users.noreply.github.com>
2022-08-11 09:28:14 +0200

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! + Fred";
+		return "Greetings from Spring Boot + Tanzu + Fred!";
 	}
 
 }
